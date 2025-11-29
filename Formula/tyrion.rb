6ad1658718ddc1867a540b3771ad3f0a0edfb00a
Dennis Vink <dennis.vink@xebia.com>
2025-11-29 12:28:52 -0500

    Bumped version

--- Formula/tyrion.rb
+++ Formula/tyrion.rb
@@ -1,8 +1,8 @@
 class Tyrion < Formula
   desc "Tiny Python-derived scripting language"
   homepage "https://github.com/dennisvink/tyrion"
-  url "https://github.com/dennisvink/tyrion/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "c7b01276356c993a5c5ca87ac53d96f46eb679ae431b6046a5a08b7ce2feecb2"
+  url "https://github.com/dennisvink/tyrion/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "6eb648aeaab1787410fe456bd1974716e7f9191aafed5fbe29c39cf901e5b1ec"
   license "MIT"
 
   depends_on "rust" => :build
