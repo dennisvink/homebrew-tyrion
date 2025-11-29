556dfe6970f9cef7399316dc07bdb48a37d5328e
Dennis Vink <dennis.vink@xebia.com>
2025-11-28 19:16:38 -0500

    Updated Formula with better test

--- Formula/tyrion.rb
+++ Formula/tyrion.rb
@@ -1,5 +1,5 @@
 class Tyrion < Formula
-  desc "Tyrion language compiler"
+  desc "A tiny programming language"
   homepage "https://github.com/dennisvink/tyrion"
   url "https://github.com/dennisvink/tyrion/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "a8f033ac762393b802dc3789398834bcdc977bc9f9b18ace0776a9e8e628ad43"
@@ -12,6 +12,10 @@ class Tyrion < Formula
   end
 
   test do
-    system "#{bin}/tyrion", "--help"
+    (testpath/"hello.ty").write <<~EOS
+      print("hi")
+    EOS
+    system "#{bin}/tyrion", "hello.ty", "hello_bin"
+    assert_equal "hi\n", shell_output("./hello_bin")
   end
 end
