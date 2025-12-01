f354bc04912023456b9cb1bc69f9d02d73bdc69d
Dennis Vink <dennis.vink@xebia.com>
2025-12-01 08:55:43 -0500

    v0.3.9

--- Formula/tyrion.rb
+++ Formula/tyrion.rb
@@ -1,14 +1,17 @@
 class Tyrion < Formula
   desc "Tiny Python-derived scripting language"
   homepage "https://github.com/dennisvink/tyrion"
-  url "https://github.com/dennisvink/tyrion/archive/refs/tags/v0.3.8.tar.gz"
-  sha256 "96b62c8975ab2bbdb754ec4ca7c98a2fc7ee9d2c0dd75cb6eefb2e188caa0804"
+  url "https://github.com/dennisvink/tyrion/archive/refs/tags/v0.3.9.tar.gz"
+  sha256 "f2cda0daf2b1ffbccccf1d88223b5913bdc00bfe5c76bf2a7d06f67d06fed623"
   license "MIT"
 
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
+
+    rm_rf "target"
+    (share/"tyrion_src").install Dir["*"]
   end
 
   test do
